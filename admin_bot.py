e8f8620e52ee8f9e1a1321caea9afb3c67b056eb
m1 <uzbzarif@gmail.com>
2024-08-24 10:48:11 +0500

    send results from monlytype

--- admin_bot.py
+++ admin_bot.py
@@ -1,7 +1,7 @@
 import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-
+from main import get_wpm_accuracy,get_user_info,get_users_wpm_accuracy
 
 async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
@@ -12,8 +12,16 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await context.bot.send_message(chat_id=GROUP_CHAT_ID, text="Hello, everyone!")
 
 async def send_results(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    users = get_user_info('monkeytype.csv')
+
+
+
+    users_wpm_accuracy = get_users_wpm_accuracy(users,15)
+    results=''
+    for idx,user in enumerate(users_wpm_accuracy):
+        results+=f'{idx+1}. {user["full_name"]}\t WPM: {user["wpm"]} Accuracy: {user["accuracy"]}\n'
     # Send a message to the group
-    await context.bot.send_message(chat_id=GROUP_CHAT_ID, text="Hello, everyone!")
+    await context.bot.send_message(chat_id=GROUP_CHAT_ID, text=results)
 # GROUP chat ID
 GROUP_CHAT_ID =-1002190225722
 TOKEN = os.environ['TOKEN']
