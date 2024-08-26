90c13960861311fd8175cc625a47f659755d3a3b
khakimovSamdu <allamurodxakimov@gmail.com>
2024-08-26 18:43:16 +0500

    set webhook

--- set_webhook.py
+++ set_webhook.py
@@ -1,9 +1,13 @@
 import requests
 import os
+from telegram import Bot
+from html2image import Html2Image
 
 TOKEN = os.environ['TOKEN']
 WEBHOOK_URL = f"https://allamurod.pythonanywhere.com/"
 
-# Set webhook
 response = requests.get(f"https://api.telegram.org/bot{TOKEN}/setWebhook?url={WEBHOOK_URL}")
 print(response.status_code)
+bot = Bot(TOKEN)
+bot.delete_webhook()
+
